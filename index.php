4c273837517e0c2dc6d9fc7cec96e6067e1e8dcd
Jesse Rodrigues Bastos <jesse.r.bastos@gmail.com>
2020-01-26 18:56:05 -0300

    Alteracoes

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?PHP
-	echo "My first Project using Git";
-	echo "A new line insert in file.";
+	echo "My first Project using GitHUB Folder";
+	echo "A new line insert in file. in 3";
+	echo "A new line in 4";
 ?>
